31297c0d13a53ce0c359fab4d680a9961f6b2165
HideonBlack <jinyu_er@icloud.com>
2020-12-24 19:20:26 +0800

    Update RSA-求m.py

--- RSA-求m.py
+++ RSA-求m.py
@@ -9,6 +9,6 @@ phi_n=(p-1)*(q-1)
 d=gmpy2.invert(e,phi_n)
 m=pow(c,d,n)
 #print("十进制:\n%s"%m)
-m_hex=hex(m)[2:]
+m_hex=hex(m)[2:]    #[2:]把0x切了
 #print("十六进制:\n%s"%(m_hex))
 print(binascii.a2b_hex(m_hex).decode("utf-8")) #a2b_hex用于十六进制转字符串
